00168440202d0e6a2326e12fcc28224876670e73
Vitor-Goncalvees <12300128@aluno.cotemig.com.br>
2024-06-08 01:49:44 -0300

    Add files via upload

---
+++ proj_software/Home/home.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Página inicial</title>
+</head>
+<body>
+    <h1>Página Home</h1>
+<a href="logout.php">Sair</a>
+<br>
+<a href="index2.php">Ver Pedidos</a>
+<br>
+<a href="pedidos_ad">Ver Pedidos Adiados</a>
+<br>
+<?php
+
+?>
+</body>
+</html>
+
